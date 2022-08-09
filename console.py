d34ef99cd382a76f10243c0e237dce52cdd34bf3
gerardomp18 <gerardomarinparra18@gmail.com>
2022-08-09 18:10:31 -0500

    fix: correction of format of all

--- console.py
+++ console.py
@@ -227,8 +227,7 @@ class HBNBCommand(cmd.Cmd):
         else:
             for k, v in storage._FileStorage__objects.items():
                 print_list.append(str(v))
-
-        print(print_list)
+        print('[%s]' % ', '.join(print_list))
 
     def help_all(self):
         """ Help information for the all command """
