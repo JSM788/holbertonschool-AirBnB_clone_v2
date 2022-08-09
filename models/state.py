a5bb8b68b08ad54dd24d0e4440482e4792c1775f
Juan Salinas <salinasjuan788@gmail.com>
2022-08-09 11:15:48 -0700

    Updating files

--- models/state.py
+++ models/state.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from sqlalchemy import Column, String
 
+"""
+class State(BaseModel):
+    
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+"""
 
 class State(BaseModel):
     """ State class """
--- models/user.py
+++ models/user.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel
+from sqlalchemy import Column, String
+
+"""
+class User(BaseModel):
+
+    __tablename__ = 'users'
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
+"""
 
 
 class User(BaseModel):
