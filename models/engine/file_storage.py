a56d12b6f278403c1d5ea3fc30b658ef331cbe4b
gerardomp18 <gerardomarinparra18@gmail.com>
2022-08-09 10:00:32 -0500

    fix: modification of the all method to obtain a filter of the classes and implementation of the delete method to eliminate the object from the dictionary

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,16 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if cls is None:
+            return FileStorage.__objects
+        else:
+            dictionary = {}
+            for key, value in self.__objects.items():
+                if value.__class__ == cls:
+                    dictionary[key] = value
+            return dictionary
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -45,6 +52,14 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        if obj is None:
+            pass
+        else:
+            key = obj.__class__.__name__ + "." + obj.id
+            del self.__objects[key]
+            self.save()
