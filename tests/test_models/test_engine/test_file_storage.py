6c89cef47d6c04216e94c94ab97603c0e0f7345c
gerardomp18 <gerardomarinparra18@gmail.com>
2022-08-09 10:40:27 -0500

    fix: revert documentation changes

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -18,10 +18,10 @@ class test_fileStorage(unittest.TestCase):
             del storage._FileStorage__objects[key]
 
     def tearDown(self):
-        """ Remove storage file at end of tests """
+        """ test to Remove storage file at end of tests """
         try:
             os.remove('file.json')
-        except Exception:
+        except:
             pass
 
     def test_obj_list_empty(self):
