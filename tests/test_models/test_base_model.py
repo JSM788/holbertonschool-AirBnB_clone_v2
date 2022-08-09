8c92eb40d9ad707a20f72f96dcf0a8665b2d6806
gerardomp18 <gerardomarinparra18@gmail.com>
2022-08-09 10:27:58 -0500

    fix: test modification by error of pycodestyle exception was added

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -22,9 +22,10 @@ class test_basemodel(unittest.TestCase):
         pass
 
     def tearDown(self):
+        """ """
         try:
             os.remove('file.json')
-        except:
+        except Exception:
             pass
 
     def test_default(self):
