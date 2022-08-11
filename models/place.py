430d4f18d6bf32c838192f52542a021ccc1378b1
gerardomp18 <gerardomarinparra18@gmail.com>
2022-08-10 21:13:25 -0700

    Adding validation to make the relationship with dbStorage or filestorage

--- models/place.py
+++ models/place.py
@@ -16,9 +16,22 @@ class Place(BaseModel, Base):
     name = Column(String(128), nullable=False)
     description = Column(String(1024), nullable=True)
     number_rooms = Column(Integer, nullable=False, default=0)
-    number_bathrooms = Column(Integer, nullable=False, default=0) 
+    number_bathrooms = Column(Integer, nullable=False, default=0)
     max_guest = Column(Integer, nullable=False, default=0)
     price_by_night = Column(Integer, nullable=False, default=0)
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
     amenity_ids = []
+
+    if os.getenv("HBNB_TYPE_STORAGE") == "db":
+        reviews = relationship('Review', backref='state', cascade='all,delete')
+    else:
+        @property
+        def reviews(self):
+            """ Creation of getter of reviews """
+            newList = []
+            allClass = models.storage.all(Review)
+            for key, value in allClass.items():
+                if value.place_id == self.id:
+                    newList.append(value)
+            return newList
