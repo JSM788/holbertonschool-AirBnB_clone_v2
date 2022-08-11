b04a5bc3ed70f9f5369f007de9c7af6e1eae81e1
Juan Salinas <salinasjuan788@gmail.com>
2022-08-10 20:06:06 -0700

    Fixing merge conflict

--- models/city.py
+++ models/city.py
@@ -1,21 +1,9 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-<<<<<<< HEAD
-from models.base_model import BaseModel
-from sqlalchemy import Column, String, ForeignKey
-=======
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
->>>>>>> 0d16544016d37875bd2abdc66c4ef3cc152ddbd2
 
-"""
-class City(BaseModel):
-    
-    __tablename__ = 'cities'
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-"""    
 
 class City(BaseModel, Base):
     """Representation of city """
--- models/state.py
+++ models/state.py
@@ -1,23 +1,12 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-<<<<<<< HEAD
-from models.base_model import BaseModel
-from sqlalchemy import Column, String
-=======
 from models.base_model import BaseModel, Base
 from sqlalchemy import String, Column
 from sqlalchemy.orm import relationship
 import models
 from models.city import City
 from os import getenv
->>>>>>> 0d16544016d37875bd2abdc66c4ef3cc152ddbd2
 
-"""
-class State(BaseModel):
-    
-    __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
-"""
 
 class State(BaseModel, Base):
     """ State class """
