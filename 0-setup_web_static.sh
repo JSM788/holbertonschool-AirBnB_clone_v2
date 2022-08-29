5cdfd2dc207019c53bbc51d432991ea6191779db
JSM788 <salinasjuan788@gmail.com>
2022-08-28 22:16:33 -0500

    Fixing errors

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,22 +1,26 @@
 #!/usr/bin/env bash
-# Update package
-sudo apt update
-# Install nginx
-sudo apt install -y nginx
-#create directory
-sudo mkdir -p /data/
-sudo mkdir -p /data/web_static/
-sudo mkdir -p /data/web_static/releases/
-sudo mkdir -p /data/web_static/shared/
-sudo mkdir -p /data/web_static/releases/test/
-#create a fake html file
-sudo echo $'<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tHolberton School\n\t</body>\n</html>' | sudo tee /data/web_static/releases/test/index.html
-#create a symbolic link
+## Installing configuration
+
+## Update and install nginx
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get install nginx -y
+
+## Create a folders
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
+
+## Create a file index.html
+sudo echo "Hello World" | sudo tee /data/web_static/releases/test/index.html
+
+## Create a simbolink link
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-#Give ownership
-sudo chown -R ubuntu:ubuntu /data/
-#configuration to serve the content
-new_string="a location /hbnb_static\n\t{\n\t\talias /data/web_static/current;\n\t}"
-sudo sed -i "/server_name _;/$new_string" /etc/nginx/sites-available/default
-#restart nginx
+
+## Owner and group belong to ubuntu user
+sudo chown -R ubuntu:ubuntu /data
+
+## Update the nginx configuration
+new_string="a \\\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}"
+sudo sed -i "/listen 80 default_server;/$new_string" /etc/nginx/sites-available/default
+
+## Restarting the nginx service
 sudo service nginx restart
