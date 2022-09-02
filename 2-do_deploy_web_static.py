b313c78290673c1958897806fcdcef8a54dd2dec
JSM788 <salinasjuan788@gmail.com>
2022-09-01 21:29:35 -0500

    Uploading file

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -2,26 +2,34 @@
 """This script (based on the file 1-pack_web_static.py)
 that distributes an archive to your web servers, using the function do_deploy"""
 from fabric.api import *
-import pathlib
+from os import path
 
 env.hosts = ['54.89.191.220', '23.20.55.144']
 
-
 def do_deploy(archive_path):
     """Uncompress the archive"""
-    if pathlib.Path(archive_path).exists() == False:
+    if not path.exists(archive_path):
         return False
     else:
-        path_tmp = "/tmp/web_static_{}.tgz"
-        path_data = "/data/web_static/releases/web_static_{}/"
-    
-        put(archive_path, "/tmp")
+        # versions/web_static_20170315003959.tgz
+        with_tgz = archive_path.split("/")
+        with_tgz = with_tgz[1]
+        
+        no_tgz = with_tgz.split(".")
+        no_tgz = no_tgz[0]
+        
+        # /tmp/web_static_20170315003959.tgz
+        path_tmp = "/tmp/{}".format(with_tgz)
+        print("PATH_TMP")
+        # /data/web_static/releases/web_static_20170315003959/
+        path_data = "/data/web_static/releases/{}/".format(no_tgz)
+
+        put(archive_path, "/tmp/")
         run("mkdir -p {}".format(path_data))
         run("tar -xzf {} -C {}".format(path_tmp, path_data))
         run("rm {}".format(path_tmp))
-        run("mv {}/web_static/* {}".format(path_data, path_data))
-        run("rm -rf {}/web_static".format(path_data))
+        run("mv {}web_static/* {}".format(path_data, path_data))
+        run("rm -rf {}web_static".format(path_data))
         run("rm -rf /data/web_static/current")
         run("ls -s {} /data/web_static/current".format(path_data))
-
-
+        return True
