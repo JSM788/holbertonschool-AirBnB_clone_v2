13e3b0d936e1d114e84fc22c100b514b9c8f1c2a
JSM788 <salinasjuan788@gmail.com>
2022-09-17 21:15:18 -0500

    Fixing errors

--- models/base_model.py
+++ models/base_model.py
@@ -17,18 +17,16 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        if not kwargs:
-            from models import storage
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-        else:
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.utcnow()
+        self.updated_at = datetime.utcnow()
+        if kwargs:
             for key, value in kwargs.items():
                 if key == "created_at":
-                    self.created_at = datetime.strptime(value,
+                    value = datetime.strptime(value,
                                                         '%Y-%m-%dT%H:%M:%S.%f')
                 elif key == "updated_at":
-                    self.updated_at = datetime.strptime(value,
+                    value = datetime.strptime(value,
                                                         '%Y-%m-%dT%H:%M:%S.%f')
                 elif not key == "__class__":
                     setattr(self, key, value)
@@ -36,12 +34,14 @@ class BaseModel:
     def __str__(self):
         """Returns a string representation of the instance"""
         cls = (str(type(self)).split('.')[-1]).split('\'')[0]
-        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
+        dic = self.__dict__.copy()
+        dic.pop("_sa_instance_state", None)
+        return '[{}] ({}) {}'.format(cls, self.id, dic)
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
         from models import storage
-        self.updated_at = datetime.now()
+        self.updated_at = datetime.utcnow()
         storage.new(self)
         storage.save()
 
